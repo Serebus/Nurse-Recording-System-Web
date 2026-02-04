43afc6af431aa34f8f1dfc45a8572bec896b1ae3
Arcgel <darkdusty8000@gmail.com>
2026-02-05 06:44:59 +0800

    Updated Formatting

--- src/stores/authStore.js
+++ src/stores/authStore.js
@@ -12,8 +12,8 @@ export const useAuthStore = defineStore('authStore', () => {
 
   const resetFormLogin = () => {
     formLogin.value = {
-      email: "",
-      password: "",
+      email: '',
+      password: '',
     }
   }
 
@@ -23,7 +23,7 @@ export const useAuthStore = defineStore('authStore', () => {
     try {
       const response = await fetch('http://localhost:5022/api/Auth/login', {
         method: 'POST',
-        credentials: 'include', 
+        credentials: 'include',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(formLogin.value),
       })
@@ -35,7 +35,7 @@ export const useAuthStore = defineStore('authStore', () => {
         nurse.value = data.user
         localStorage.setItem('nurse', JSON.stringify(nurse.value))
         if (data.user.nurseDetails && data.user.nurseDetails.nurseId) {
-          localStorage.setItem('nurseId', data.user.nurseDetails.nurseId);
+          localStorage.setItem('nurseId', data.user.nurseDetails.nurseId)
         }
         return true
       } else {
@@ -50,21 +50,21 @@ export const useAuthStore = defineStore('authStore', () => {
 
   const logout = async () => {
     try {
-    await fetch('http://localhost:5022/api/Auth/logout', {
-      method: 'POST',
-      credentials: 'include',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({}) 
-    })
-  } catch (error) {
-    console.error('Logout error', error)
-  } finally {
-    // Always clear client state, even if server errors out
-    nurse.value = null
-    localStorage.removeItem('nurse')
-    localStorage.removeItem('nurseId')
-    // Optional: Redirect to login
-  }
+      await fetch('http://localhost:5022/api/Auth/logout', {
+        method: 'POST',
+        credentials: 'include',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({}),
+      })
+    } catch (error) {
+      console.error('Logout error', error)
+    } finally {
+      // Always clear client state, even if server errors out
+      nurse.value = null
+      localStorage.removeItem('nurse')
+      localStorage.removeItem('nurseId')
+      // Optional: Redirect to login
+    }
   }
 
   return {
